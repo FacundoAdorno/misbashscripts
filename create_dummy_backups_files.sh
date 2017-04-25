d80c5783802a4b056a13b63496374f3d7d7a3ac1
FacundoAdorno <adorno.facundo@gmail.com>
2017-04-25 11:17:03 -0300

    Algunos cambios a los scripts

--- create_dummy_backups_files.sh
+++ create_dummy_backups_files.sh
@@ -3,10 +3,10 @@
 
 TMP_FILE=/tmp/test_file_safe_to_delete.txt
 #Default number of copies to do
-N_COPIES=30
+N_COPIES=90
 #Default directory where put the copies
 DIR_COPIES=/tmp/prueba
-#Set to true if you want to create the files with changing  creation date 
+#Set to true if you want to create the files with modified creation date 
 CHANGE_CREATION_DATE=0
 #Set to true if you want to decrement days of creation instead of adding...
 DECREMENT_DATES=0
@@ -47,7 +47,7 @@ fi
 for i in `seq -w 1 $N_COPIES`
 do
     FICT_DATE=`date -d "$DATESTR $DATE_OPERATOR $i days" +%Y-%m-%d_%H%M`
-    cp "$TMP_FILE" "$DIR_COPIES/backup_$FICT_DATE.bkp"
+    cp "$TMP_FILE" "$DIR_COPIES/backup_$FICT_DATE.bkp.gz"
     if [ $CHANGE_CREATION_DATE -gt 0 ]; then 
     	CREATION_DATE_STR=`date -d "$DATESTR $DATE_OPERATOR $i days" +%Y-%m-%d`
 	touch -a -m -d "$CREATION_DATE_STR" $DIR_COPIES/backup_$FICT_DATE.bkp
